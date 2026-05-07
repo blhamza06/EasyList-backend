0ede61d1228968f87eb92af477f28ce9d60b33b7
Hamza Blaybel <hamza.blaybel@student.htw-berlin.de>
2026-05-07 17:37:37 +0200

    CORS hinzugefügt für Render Deployment

--- src/main/java/de/htw_berlin/EasyList/ShoppingItemController.java
+++ src/main/java/de/htw_berlin/EasyList/ShoppingItemController.java
@@ -1,11 +1,13 @@
 package de.htw_berlin.EasyList;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class ShoppingItemController {
 
