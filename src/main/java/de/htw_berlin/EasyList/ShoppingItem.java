5c59dc4e6ffef014b2827c39cc6768e503acefa4
Mehmet Albayrak <mehmetalbayrak@MacBookAir.fritz.box>
2026-06-09 21:54:54 +0200

    feat: ShoppingItem zur JPA Entity gemacht, ShoppingItemRepository erstellt

--- src/main/java/de/htw_berlin/EasyList/ShoppingItem.java
+++ src/main/java/de/htw_berlin/EasyList/ShoppingItem.java
@@ -1,14 +1,25 @@
 package de.htw_berlin.EasyList;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "shopping_items")
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class ShoppingItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String name;
     private int quantity;
+    private String unit;
     private String category;
 }
